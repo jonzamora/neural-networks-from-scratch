2d35b3f253e6adf93edf5b02fceac678755658e7
Jonathan Zamora <jonzamora18@gmail.com>
2024-01-31 23:55:10 -0800

    format update

--- src/ch3-adding-layers/dense_layer.py
+++ src/ch3-adding-layers/dense_layer.py
@@ -1,17 +1,20 @@
-# Dense Layer
+"""
+This is a Dense Layer implementation with NumPy
+"""
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-class Layer_Dense:
 
+class DenseLayer:
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.output = None
 
     # Forward Pass
     def forward(self, inputs):
@@ -19,14 +22,20 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-# Create dataset
-X, y = spiral_data(samples=100, classes=3)
+def main():
 
-# Create a Dense layer with 2 input features and 3 output values
-dense1 = Layer_Dense(n_inputs=2, n_neurons=3)
+    # Create dataset
+    X, y = spiral_data(samples=100, classes=3)
 
-# Perform a forward pass of our training data through this layer
-dense1.forward(X)
+    # Create a Dense layer with 2 input features and 3 output values
+    dense1 = DenseLayer(n_inputs=2, n_neurons=3)
 
-# Let's see output of the first few samples:
-print(dense1.output[:5])
+    # Perform a forward pass of our training data through this layer
+    dense1.forward(X)
+
+    # Let's see output of the first few samples:
+    print(dense1.output[:5])
+
+
+if __name__ == "__main__":
+    main()
