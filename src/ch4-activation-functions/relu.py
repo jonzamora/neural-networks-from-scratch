ef6399f087aebb2c31a7d2e3f59a4d0df210d180
Jonathan Zamora <jonzamora18@gmail.com>
2024-02-01 23:29:29 -0800

    add relu.py

---
+++ src/ch4-activation-functions/relu.py
@@ -0,0 +1,16 @@
+"""
+Rectified Linear Units (ReLU) Activation Function
+Version 1: Simple implementation
+"""
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+
+output = []
+
+for i in inputs:
+    if i > 0:
+        output.append(i)
+    else:
+        output.append(0)
+
+print(output)
